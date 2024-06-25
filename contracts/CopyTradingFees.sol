0df47a084bfa307d549b92f324c5d91234208bc1
andoan <secret.andoan@gmail.com>
2024-06-25 10:55:00 +0700

    refactor

--- contracts/CopyTradingFees.sol
+++ contracts/CopyTradingFees.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/token/common/ERC2981.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import {ICopyTradingFees} from "contracts/interfaces/ICopyTradingFees.sol";
